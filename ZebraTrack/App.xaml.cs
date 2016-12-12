572c2a89a9e96a4819a6a7c560df3bb8db30b3db
Martin Haesemeyer <m.haesemeyer@gmail.com>
2016-12-12 13:25:56 -0500

    Initial project commit

---
+++ ZebraTrack/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+using MHApi.Utilities;
+using ipp;
+
+namespace ZebraTrack
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+            DispatcherHelper.Initialize();
+            core.ippInit();
+        }
+    }
+}
