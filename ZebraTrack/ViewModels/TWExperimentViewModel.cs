1aff87d4a6d7cc5ffb634ce705752bea20ee6042
Martin Haesemeyer <m.haesemeyer@gmail.com>
2016-12-14 15:53:38 -0500

    Added view and view-model to represent track and write experiment and hooked them up to the MainViewModel's experiment logic.

---
+++ ZebraTrack/ViewModels/TWExperimentViewModel.cs
@@ -0,0 +1,60 @@
+﻿/*
+Copyright 2016 Martin Haesemeyer
+   Licensed under the MIT License, see License.txt.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
+
+using System;
+using System.IO;
+using ZebraTrack.Experiments;
+
+using MHApi.GUI;
+
+namespace ZebraTrack.ViewModels
+{
+    /// <summary>
+    /// ViewModel for simple TrackAndWrite experiment
+    /// </summary>
+    class TWExperimentViewModel : ViewModelBase, IExperimentVM
+    {
+        /// <summary>
+        /// The length of the experiment in seconds
+        /// </summary>
+        int _experimentLength;
+
+        public TWExperimentViewModel()
+        {
+            ExperimentLength = 30 * 60;
+        }
+
+        /// <summary>
+        /// The length of the experiment in seconds
+        /// </summary>
+        public int ExperimentLength
+        {
+            get
+            {
+                return _experimentLength;
+            }
+            set
+            {
+                if (value < 1)
+                    throw new ArgumentOutOfRangeException(nameof(ExperimentLength), "Experiment needs to be at least 1s long");
+                _experimentLength = value;
+                RaisePropertyChanged(nameof(ExperimentLength));
+            }
+        }
+
+        public ExperimentBase CreateExperiment(string name, string fishID)
+        {
+            string folder = Path.Combine(Properties.Settings.Default.SavePath,"TrackAndWrite");
+            return new TrackAndWrite(_experimentLength, Properties.Settings.Default.FrameRate, folder, name, fishID);
+        }
+    }
+}
---
+++ ZebraTrack/Views/TWExperimentView.xaml.cs
@@ -0,0 +1,52 @@
+﻿/*
+Copyright 2016 Martin Haesemeyer
+   Licensed under the MIT License, see License.txt.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MHApi.GUI;
+using ZebraTrack.ViewModels;
+
+namespace ZebraTrack.Views
+{
+    /// <summary>
+    /// Interaction logic for TWExperimentView.xaml
+    /// </summary>
+    public partial class TWExperimentView : WindowAwareView
+    {
+        TWExperimentViewModel _viewModel;
+
+        public TWExperimentView()
+        {
+            InitializeComponent();
+            _viewModel = ViewModel.Source as TWExperimentViewModel;
+        }
+
+        private void WindowAwareView_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Make the MainViewModel aware of the fact that this experiment type has been selected
+            MainViewModel.ExperimentViewModel = _viewModel;
+        }
+    }
+}
