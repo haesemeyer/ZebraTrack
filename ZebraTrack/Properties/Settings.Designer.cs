aba4f95fbe793f8db51c7bb573a11e6627f18b31
Martin Haesemeyer <m.haesemeyer@gmail.com>
2020-07-30 16:37:24 -0400

    Changed system resolution

--- ZebraTrack/Properties/Settings.Designer.cs
+++ ZebraTrack/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ZebraTrack.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -52,7 +52,7 @@ namespace ZebraTrack.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("24")]
+        [global::System.Configuration.DefaultSettingValueAttribute("5")]
         public int PixelsPermm {
             get {
                 return ((int)(this["PixelsPermm"]));
